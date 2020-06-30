8028d0eaf8d84adf83e102c2f4b054cfe94b2a9f
Christian Acuna <christiandeeplearning0828@gmail.com>
2020-06-30 13:27:42 -0700

    Add Google Cloud Run config with Dockerfile, Procfile, requirements'

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
@@ -17,4 +18,8 @@ def hello_world():
         image = file.read()
         dog_breed = get_dog_breed_name(image_bytes=image)
         print(dog_breed)
-        return render_template('results.html', breed=dog_breed)
+
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
