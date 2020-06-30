54cb14cfb3981933084838c17960826e64c5661c
Christian Acuna <christian_acuna@Christians-Mini.fios-router.home>
2020-06-30 11:40:54 -0700

    update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def hello_world():
     if request.method == 'GET':
         return render_template('index.html', value='hi')
     if request.method == 'POST':
-        return render_template('results.html')
+        return render_template('results.html', breed="Husky")
